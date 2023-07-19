09a56d335e744caccb775522c0bdf42d7afb6434
hgarni <hgarni@hotmail.com>
2023-07-19 16:45:07 -0300

    ajustes 751_HT_lighthgbm_wf01.r

--- src/workflow-semillerio/751_HT_lightgbm_wf01.r
+++ src/workflow-semillerio/751_HT_lightgbm_wf01.r
@@ -27,9 +27,9 @@ options(error = function() {
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "HT7510_wf01"
+PARAM$experimento <- "HT7510_WF_01"
 
-PARAM$exp_input <- "TS7410_wf01"
+PARAM$exp_input <- "TS7410_WF_01"
 
 # En caso que se haga cross validation, se usa esta cantidad de folds
 PARAM$lgb_crossvalidation_folds <- 5
