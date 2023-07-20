41bf2cf5abe5947992685a08c890377022d99424
hgarni <hgarni@hotmail.com>
2023-07-20 01:10:15 -0300

    correccion en los 741 TS wf

--- src/workflow-semillerio/741_TS_training_strategy_wf02.r
+++ src/workflow-semillerio/741_TS_training_strategy_wf02.r
@@ -14,7 +14,7 @@ PARAM <- list()
 PARAM$experimento <- "TS7410_WF_02"
 
 #PARAM$exp_input <- "FE7310"
-PARAM$exp_input <- "FE6310_WF"
+PARAM$exp_input <- "FE7310_WF"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
